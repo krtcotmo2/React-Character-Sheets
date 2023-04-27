ba82661b323f25ebb661fce2a67bdeec811996e1
Kurt Cooney <KCooney@interdev.com>
2023-04-26 22:39:28 -0400

    packages and misc smaller updates

---
+++ src/views/select-character/select-character.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from 'react';
+import { CharacterList } from '../../components/load-character/char-list';
+
+export const SelectCharacter: React.FC = (): JSX.Element => {
+   
+    return(
+       <CharacterList/>
+    )
+}
