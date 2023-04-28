ded7f7269e04fb4ce32271b325f3e09cc809ead5
Kurt Cooney <KCooney@interdev.com>
2023-04-27 22:38:20 -0400

    collapsible rows in stats

--- src/components/collapsible-row/collapsible-row.tsx
+++ src/components/collapsible-row/collapsible-row.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import { Modifier } from '../../interfaces/modifier';
+import { Grid, } from '@mui/material';
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
+import { useStyles } from './collapsible-row-styles';
+import "../../App.css";
 
 interface RowProps {
     title: string;
@@ -9,11 +13,22 @@ interface RowProps {
 }
 
 export const CollapsibleRow: React.FC<RowProps> = (props: RowProps): JSX.Element => {
+    const [hidden, setHidden] = useState(true);
+    const clickIcon = (arg: any) => {
+        setHidden(!hidden);
+    }
+    const { classes } = useStyles();
     const {title, value, breakdown, includeStatBonus} = props;
     return (
-        <>
-            <p>{title}: {value} {includeStatBonus ? calcBonus(value) : ''}</p>
-        </>
+        <Grid container>
+            <Grid item>
+                {title}: {value} {includeStatBonus ? calcBonus(value) : ''}
+                <KeyboardArrowDown onClick={clickIcon} className={`${hidden ? '' : 'rotated'} ${classes.iconPadded}`}/>
+            </Grid>
+            <Grid container direction={'column'} className={`${hidden ? 'hidden' : ''}`} >
+                {breakdown.map( mod => (<Grid container item className={`${classes.breakDownSection}`}>{mod.score} - {mod.modDesc}</Grid>))}
+            </Grid>
+        </Grid>
     )
 }
 
@@ -21,3 +36,4 @@ export const calcBonus = (statValue: number): string => {
     const bonus = Math.floor((statValue - 10) / 2);
     return bonus >= 0 ? `+${bonus}`: bonus.toString();
 }
+
--- src/views/stats/stats.tsx
+++ src/views/stats/stats.tsx
@@ -8,13 +8,13 @@ export const CharacterStats: React.FC = (): JSX.Element => {
     const char: Stat = useSelector(state => store.getState().stats);
 
     return (
-        <>
+        <div style={{padding:'72px 24px 0px 24px'}}>
             <CollapsibleRow title='Strength' value={char.str.value} breakdown={char.str.breakdown} includeStatBonus= {true}/>
             <CollapsibleRow title='Dexterity' value={char.dex.value} breakdown={char.dex.breakdown} includeStatBonus= {true}/>
             <CollapsibleRow title='Constitution' value={char.con.value} breakdown={char.con.breakdown} includeStatBonus= {true}/>
             <CollapsibleRow title='Intelligence' value={char.int.value} breakdown={char.int.breakdown} includeStatBonus= {true}/>
             <CollapsibleRow title='Wisdom' value={char.wis.value} breakdown={char.wis.breakdown} includeStatBonus= {true}/>
             <CollapsibleRow title='Charisma' value={char.chr.value} breakdown={char.chr.breakdown} includeStatBonus= {true}/>
-        </>
+        </div>
     )
 } 
