d3cc8aa34661e631c2f13fc424caed4ac5dcc177
Kurt Cooney <KCooney@interdev.com>
2023-07-09 09:30:58 -0400

    refactor modifier row and view

--- src/components/modifier/modifier-row.tsx
+++ src/components/modifier/modifier-row.tsx
@@ -1,64 +1,41 @@
 import { Button, FormControl, Grid, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 import React, { useEffect, useState } from 'react';
 import { Modifier } from "../../interfaces/modifier";
-import { getModDescription, getModifiers, getReducer } from "./business-logic/modifier-helper";
 import Delete from '@mui/icons-material/Delete';
 
 export interface ModifierProps {
-    whatIsMod?: string;
-    modified?: any;
+    modifier?: Modifier;
     saveFunction?: () => void;
+    index: number;
 }
 
 
 export const ModifierRow: React.FC<ModifierProps> = (props: ModifierProps): JSX.Element => {
-    const emptyModifiers: Modifier[] = [];
-    const {modified, whatIsMod} = props;
-    const [modifiers, setModifiers] = useState(emptyModifiers);
-
-    useEffect(() => {
-        const theReducer = getReducer(whatIsMod || '');
-        const mods = getModifiers(modified, theReducer);
-        setModifiers(mods);
-    },[]);
+    const {modifier, index} = props;
     return <>
-        <Grid direction='column' rowGap={2} style={{display:"flex"}}>
-            <Grid item style={{alignSelf:'center'}}>
-                <h1>{props.modified}</h1>
+        <Grid container item style={{width:'fit-content'}} columnGap={2} alignItems='center'>                            
+            <Grid item  style={{display:'flex', alignItems:'center'}}>
+                <span style={{paddingRight: '12px'}}>{index+1}.</span>
+                <TextField
+                    required    
+                    type="number"
+                    value={modifier?.score}
+                    InputProps={{ inputProps: {step: "1" } }}
+                    disabled={modifier?.id===0}
+                />
             </Grid>
-            <Grid container direction='column' rowGap={2} item alignContent='center'>
-
-
-                {modifiers.map( (m: Modifier, i) => (
-                    <Grid container item style={{width:'fit-content'}} columnGap={2} alignItems='center'>                            
-                        <Grid item  style={{display:'flex', alignItems:'center'}}>
-                            <span style={{paddingRight: '12px'}}>{i+1}.</span>
-                            <TextField
-                                required    
-                                type="number"
-                                value={m.score}
-                                InputProps={{ inputProps: {step: "1" } }}
-                                disabled={m.id===0}
-                            />
-                        </Grid>
-                        <Grid item>
-                            <TextField
-                                value={m.modDesc}
-                                disabled={m.id===0}
-                            />
-                        </Grid>
-                        <Grid item>
-                            {m.id!==0 && (
-                                <Delete ></Delete>
-                            )}
-                        </Grid>
-                    </Grid>
-                ))}
+            <Grid item>
+                <TextField
+                    value={modifier?.modDesc}
+                    disabled={modifier?.id===0}
+                />
             </Grid>
-
-
+            <Grid item>
+                {modifier?.id!==0 && (
+                    <Delete ></Delete>
+                )}
+            </Grid>
         </Grid>
-            {/* {modifiers.map( (i: Modifier) => (<p>ModRow {i.score} {getModDescription(i, whatIsMod || '')}</p>))} */}
     </>
     
 };
--- src/components/modifier/modifier.tsx
+++ src/components/modifier/modifier.tsx
@@ -2,8 +2,8 @@ import { Button, Divider, FormControl, Grid, InputLabel, MenuItem, Select, TextF
 import React, { useEffect, useState } from 'react';
 import { WHATISMOD } from "../../enum/what-is-mod-type";
 import { Modifier } from "../../interfaces/modifier";
-import { useLocation } from "react-router-dom"
-import { getReducer, getStatToModify } from "./business-logic/modifier-helper";
+import { Link, useLocation } from "react-router-dom"
+import { getModifiers, getReducer, getStatToModify } from "./business-logic/modifier-helper";
 import { Character } from "../../interfaces/character";
 import { Expendable } from "../../interfaces/expendable";
 import { Feat } from "../../interfaces/feats";
@@ -12,6 +12,8 @@ import { SavingThrow } from "../../interfaces/saving-throw";
 import { RawSkill } from "../../interfaces/skills";
 import { Stat } from "../../interfaces/stat";
 import { ModifierRow } from "./modifier-row";
+import { useSelector } from "react-redux";
+import { store } from "../../redux/configure-store";
 
 interface ModifierProps {
     whatIsMod?: any;
@@ -22,14 +24,31 @@ interface ModifierProps {
 
 export const ModifierView: React.FC<ModifierProps> = (props: ModifierProps): JSX.Element => {
     const { state } = useLocation();
+    const emptyModifiers: Modifier[] = [];
+    const [modifiers, setModifiers] = useState(emptyModifiers);
+    useEffect(() => {
+        const theReducer = getReducer(state.whatIsMod || '');
+        const mods = getModifiers(state.modified, theReducer);
+        setModifiers(mods);
+    },[]);
+    const char: Character = useSelector(state => store.getState().character);
     return (
-        <>
-            
-            <Grid container direction='column' justifyContent="center" alignItems='center'>
-                <ModifierRow whatIsMod={state.whatIsMod} modified={state.modified}/>
+        <Grid container direction='column' justifyContent="center" alignItems='center'>
+            <Grid item style={{alignSelf:'center'}}>
+                <p><Link className='nonDecLink' to={`/character/overview/${char.charID}`}>{char?.charName}</Link> - {state.modified}</p>
+            </Grid>
+            <Grid direction='column' rowGap={2} style={{display:"flex"}}>
+                <Grid container direction='column' rowGap={2} item alignContent='center'>
+                    {
+                        modifiers.map( 
+                            (m: Modifier, i) => (<ModifierRow modifier={m} index={i}/>)
+                        )
+                    }
+                </Grid>
+            </Grid>
+            {/*  */}
             <Divider color='#fff' style={{width:'100%', margin: '12px 0', borderTopWidth: '2px', borderTopColor:'#6a6a6a'}}/>
             <Button style={{width:'fit-content'}} variant="contained">Add New Modifier</Button>
-            </Grid>
-        </>
+        </Grid>
     )
 };
